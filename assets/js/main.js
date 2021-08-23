7ddf36989bab8e46ab00c1107e0ac350b0e16989
imacte <imacyh@outlook.com>
2021-08-23 18:22:26 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,2 @@
 console.clear();
-console.log("\n %c Steven's Blog","color:#fff;background: linear-gradient(to right , #424242, #a9a9a9);padding:5px;border-radius: 10px;");
+console.log("\n %c Steven's Home","color:#fff;background: linear-gradient(to right , #424242, #a9a9a9);padding:5px;border-radius: 10px;");
